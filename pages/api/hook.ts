1040a6569b5657f54c99d16bad2e8883cbcaded2
Jonathan Kula <jonathan@jdkula.dev>
2020-09-15 00:35:10 -0700

    Give more logging information and make sure all emails are sent by the termination of the function

--- pages/api/hook.ts
+++ pages/api/hook.ts
@@ -9,6 +9,7 @@ const MEETING_ENDED = "meeting.ended";
 const MEETING_STARTED = "meeting.started";
 
 async function sendEmail(to, text) {
+    console.log("Sending", to, "-", text);
     await mg.messages().send({
         from: process.env.MAILGUN_FROM,
         to,
@@ -18,37 +19,34 @@ async function sendEmail(to, text) {
 
 const Hook: NextApiHandler = async (req, res) => {
     if (req.headers["authorization"] !== process.env.VERIFICATION_TOKEN) {
-        res.status(401).end();
+        res.status(401).send("Not Authorized");
         return;
     }
 
     if (req.body?.payload?.object?.id !== process.env.MEETING_ID) {
-        res.status(400).end();
+        res.status(400).send("Invalid Meeting ID");
         return;
     }
 
     const db = await mongo;
     const subscriptions = await db.collection("subscriptions").find<Subscription>().toArray();
-    const promises = [];
 
     for (const subscription of subscriptions) {
         try {
             if (req.body.event === MEETING_STARTED && subscription.start) {
-                promises.push(
-                    sendEmail(
-                        subscription.email,
-                        "Someone just joined the Squad Zoom! Join at ***REMOVED***/",
-                    ),
+                await sendEmail(
+                    subscription.email,
+                    "Someone just joined the Squad Zoom! Join at " + process.env.MEETING_URL,
                 );
             } else if (req.body.event === MEETING_ENDED && subscription.end) {
-                promises.push(sendEmail(subscription.email, "Nobody's left in the Squad Zoom."));
+                await sendEmail(subscription.email, "Nobody's left in the Squad Zoom.");
             }
         } catch (e) {
             console.log("Failed...");
         }
     }
 
-    res.end("OK");
+    res.status(200).end("OK");
 };
 
 export default Hook;
