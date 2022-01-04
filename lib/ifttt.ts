580ff5e553b623dfb667bbc84b7bbbf90e87029a
Jonathan Kula <jonathan@jdkula.dev>
2022-01-04 05:26:48 -0800

    Add logs to IFTTT messages, too

--- lib/ifttt.ts
+++ lib/ifttt.ts
@@ -1,6 +1,7 @@
 import Axios from 'axios';
 
 export default async function sendIfttt(key: string, title: string, message: string, link?: string): Promise<void> {
+    console.log(`Sending IFTTT message with key ${key}: ${message}`);
     await Axios.post(`https://maker.ifttt.com/trigger/zoom_notification/with/key/${key}`, {
         value1: title,
         value2: message,
