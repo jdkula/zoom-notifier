4b991cd56acaee59edd37ae82f66e4ad82fd424c
Jonathan Kula <jonathan@jdkula.dev>
2022-09-18 17:36:20 -0700

    Update from deprecated mailgun version

--- lib/sendEmail.ts
+++ lib/sendEmail.ts
@@ -1,10 +1,15 @@
-import mailgun, { messages } from 'mailgun-js';
+import formData from 'form-data';
+import Mailgun from 'mailgun.js';
+import type { MessagesSendResult } from 'mailgun.js/interfaces/Messages';
 
-const mg = mailgun({ apiKey: process.env.MAILGUN_API, domain: process.env.MAILGUN_DOMAIN });
+const mg = new Mailgun(formData).client({
+    username: 'api',
+    key: process.env.MAILGUN_API,
+});
 
-export async function sendEmail(to: string, text: string, subject?: string): Promise<messages.SendResponse> {
+export async function sendEmail(to: string, text: string, subject?: string): Promise<MessagesSendResult> {
     console.log('Sending', to, '-', text);
-    return await mg.messages().send({
+    return await mg.messages.create(process.env.MAILGUN_DOMAIN, {
         from: process.env.MAILGUN_FROM,
         to,
         text,
